bfc1ab2be6ee4eeb9d0dd0fdb9d5ac3b13adc3be
erozonachi <erozonachi.james@gmail.com>
2019-06-21 09:44:17 +0100

    Convert smurfs to functional component

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React, {  } from 'react';
 import Smurf from './Smurf';
 import SmurfList from './StyledComponents/SmurfList';
 import { connect } from 'react-redux';
 import { deleteSmurf } from '../actions';
 
-class Smurfs extends Component {
-  render() {
-    return (
-      <SmurfList>
-        <h1>Smurf Village</h1>
-        <ul>
-          {this.props.app.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-                delHandler={this.props.deleteSmurf}
-              />
-            );
-          })}
-        </ul>
-      </SmurfList>
-    );
-  }
+function Smurfs(props) {
+  return (
+    <SmurfList>
+      <h1>Smurf Village</h1>
+      <ul>
+        {props.app.smurfs.map(smurf => {
+          return (
+            <Smurf
+              name={smurf.name}
+              id={smurf.id}
+              age={smurf.age}
+              height={smurf.height}
+              key={smurf.id}
+              delHandler={props.deleteSmurf}
+            />
+          );
+        })}
+      </ul>
+    </SmurfList>
+  );
 }
 
 Smurf.defaultProps = {
