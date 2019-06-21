51a60c9cef2d37e944b9914904619d364c032636
erozonachi <erozonachi.james@gmail.com>
2019-06-21 09:34:00 +0100

    Create and export smurfs component

---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Smurf from './Smurf';
+import SmurfList from './StyledComponents/SmurfList';
+
+class Smurfs extends Component {
+  render() {
+    return (
+      <SmurfList>
+        <h1>Smurf Village</h1>
+        <ul>
+          {this.props.smurfs.map(smurf => {
+            return (
+              <Smurf
+                name={smurf.name}
+                id={smurf.id}
+                age={smurf.age}
+                height={smurf.height}
+                key={smurf.id}
+                delHandler={this.props.delHandler}
+              />
+            );
+          })}
+        </ul>
+      </SmurfList>
+    );
+  }
+}
+
+Smurf.defaultProps = {
+ smurfs: [],
+};
+
+export default Smurfs;
